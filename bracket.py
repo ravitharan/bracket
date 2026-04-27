2a67c76e6c5c2432046a36ea621d0ffd6a6fd84e
Ravi G Ravitharan <ravitharan@gmail.com>
2026-04-27 19:03:35 +0100

    Handle late comers and absentees, run multiple times

--- bracket.py
+++ bracket.py
@@ -4,6 +4,7 @@ import sys
 import openpyxl
 from itertools import combinations
 import random
+import copy
 
 ATTENDANCE_SHEET = "attendance"
 
@@ -21,12 +22,19 @@ COURTS_COUNT = 5
 MENS_COURT_COUNT = 3
 CHILD_COURT_INX = MENS_COURT_COUNT
 CRT5_COURT_INX = COURTS_COUNT - 1
+MAX_CRT5_PLAY_COUNT = 1
+
+MAX_LATE_VALUE = 1000
+ABSENT_VALUE = 2000
+
+ROUND1_LATE_TH = 2
+ROUND2_LATE_TH = 15
+MAX_SELECTION_RUN = 10000
 
 def retrive_teams(wb):
     ws = wb[ATTENDANCE_SHEET]
     teams = {}
     groups = [[] for i in range(6)]
-    quali_matches = []
     team_ranks = []
 
     for row in range(2, ws.max_row+1):
@@ -37,18 +45,32 @@ def retrive_teams(wb):
         player2 = ws.cell(row, 5).value
         attend2 = ws.cell(row, 6).value
         if rank and team and player1 and player2:
-            if attend1 == -1 or attend2 == -1:
-                continue
             teams[team] = {}
-            if attend1 == 1 and attend2 == 1:
-                teams[team]['ready'] = 1
-            else:
-                teams[team]['ready'] = 0
-            teams[team]['players'] = f'{player1} & {player2}'
-            teams[team]['crt5_played'] = False
-            teams[team]['score_rows'] = []
             teams[team]['rounds'] = []
-            teams[team]['wait_time'] = 1
+            if attend1 != None:
+                a1 = int(float(attend1))
+            else:
+                a1 = MAX_LATE_VALUE
+
+            if attend2 != None:
+                a2 = int(float(attend2))
+            else:
+                a2 = MAX_LATE_VALUE
+
+            if a1 < 0:
+                print(f'Error in sheet {ATTENDANCE_SHEET} at row {row}, invalid attendance value "{attend1}"')
+                exit(1)
+
+            if a2 < 0:
+                print(f'Error in sheet {ATTENDANCE_SHEET} at row {row}, invalid attendance value "{attend2}"')
+                exit(1)
+
+            if a1 == 0 or a2 == 0:
+                teams[team]['ready'] = ABSENT_VALUE
+            else:
+                teams[team]['ready'] = max(a1, a2)
+
+            teams[team]['players'] = f'{player1} & {player2}'
 
             if rank == 'wb':
                 teams[team]['group'] = 'Women & Boys'
@@ -78,34 +100,36 @@ def retrive_teams(wb):
         elif (rank % 4) == 3:
             teams[team]['group'] = 'Group D'
 
-    for group in groups:
-        matches = combinations(group, 2)
-        for match in matches:
-            quali_matches.append(list(match))
+    return teams, groups
 
-    random.shuffle(quali_matches)
-
-    return teams, groups, quali_matches
-
-def choose_matches(teams, quali_matches):
+def choose_matches(round_count, teams, quali_matches):
     rounds = [None for i in range(COURTS_COUNT)]
     available_rounds = []
     selected_teams = []
+    choosen_count = 0
 
     for [left, right] in quali_matches:
-        entry = [left, right, teams[left]['ready'], teams[right]['ready'], teams[left]['wait_time'], teams[right]['wait_time']]
+        highest_ready = max(teams[left]['ready'], teams[right]['ready'])
+        lowest_wait_time = min(teams[left]['wait_time'], teams[right]['wait_time'])
+        entry = [left, right, highest_ready, lowest_wait_time]
         available_rounds.append(entry)
 
+    #print(f'\nRound count {round_count}')
     inx = MENS_COURT_COUNT - 1
     if available_rounds:
-        available_rounds.sort(key=lambda k: (k[2]+k[3], k[4]+k[5], k[4], k[5]), reverse=True)
+        available_rounds.sort(key=lambda k: (-1 * k[2], lowest_wait_time), reverse=True)
         for i in range(len(available_rounds)):
             left = available_rounds[i][0]
             right = available_rounds[i][1]
-            #print(f'avail {left} {right} {available_rounds[i][2]} {available_rounds[i][3]}')
+            #print(f'{i}: avail {left} {right} {available_rounds[i][2]} {available_rounds[i][3]}')
+
             if left in selected_teams or right in selected_teams:
                 continue
-            if available_rounds[i][4] == 0 or available_rounds[i][5] == 0:
+
+            if available_rounds[i][3] == 0:
+                continue
+
+            if teams[left]['ready'] == ABSENT_VALUE or teams[right]['ready'] == ABSENT_VALUE:
                 continue
 
             if teams[left]['children'] and teams[right]['children']:
@@ -113,17 +137,27 @@ def choose_matches(teams, quali_matches):
                     rounds[CHILD_COURT_INX] = [left, right]
                     selected_teams.append(left)
                     selected_teams.append(right)
+                    choosen_count += 1
+                    #print(f'Choosen {i} {left} {right}')
             else:
-                if not rounds[CRT5_COURT_INX] and \
-                    not teams[left]['crt5_played'] and not teams[right]['crt5_played']:
-                    rounds[CRT5_COURT_INX] = [left, right]
-                    selected_teams.append(left)
-                    selected_teams.append(right)
-                elif (inx >= 0):
+                if (inx >= 0):
                     rounds[inx] = [left, right]
                     selected_teams.append(left)
                     selected_teams.append(right)
                     inx -= 1
+                    choosen_count += 1
+                    #print(f'Choosen {i} {left} {right}')
+                elif not rounds[CRT5_COURT_INX] and \
+                    (teams[left]['crt5_played'] < MAX_CRT5_PLAY_COUNT) and (teams[right]['crt5_played'] < MAX_CRT5_PLAY_COUNT):
+                    rounds[CRT5_COURT_INX] = [left, right]
+                    selected_teams.append(left)
+                    selected_teams.append(right)
+                    choosen_count += 1
+                    #print(f'Choosen {i} {left} {right}')
+
+            if choosen_count == COURTS_COUNT:
+                break
+
 
     return rounds
 
@@ -136,8 +170,8 @@ def update_chosen_teams(teams, quali_matches, rounds):
             teams[match[1]]['wait_time'] = 0
 
             if court == CRT5_COURT_INX:
-                teams[match[0]]['crt5_played'] = True
-                teams[match[1]]['crt5_played'] = True
+                teams[match[0]]['crt5_played'] += 1
+                teams[match[1]]['crt5_played'] += 1
 
             selected_teams.append(match[0])
             selected_teams.append(match[1])
@@ -167,8 +201,12 @@ def update_schedule(wb, teams, quali_rounds):
             if court == 0:
                 schedule_row.append("")
             if match:
-                schedule_row.append(match[0])
-                schedule_row.append(match[1])
+                left = match[0]
+                right = match[1]
+                teams[left]['rounds'].append((seq+1, court+1))
+                teams[right]['rounds'].append((seq+1, court+1))
+                schedule_row.append(left)
+                schedule_row.append(right)
                 schedule_row.append("")
             else:
                 schedule_row.append("")
@@ -198,6 +236,9 @@ def update_score_sheet(wb, groups, teams, quali_rounds):
     ws.append(title)
     row = 2
 
+    for team in teams:
+        teams[team]['score_rows'] = []
+
     for seq, rounds in enumerate(quali_rounds):
         ws.append([])
         row += 1
@@ -224,17 +265,22 @@ def update_score_sheet(wb, groups, teams, quali_rounds):
     ws.append([])
 
     for team in teams:
-        points_sum_eqn = '=sum('
-        score_sum_eqn = '=sum('
-        for (side, row) in teams[team]['score_rows']:
-            if side == 'left':
-                points_sum_eqn += f'{LEFT_POINTS_COL}{row}, '
-                score_sum_eqn += f'{LEFT_SCORE_COL}{row}, '
-            elif side == 'right':
-                points_sum_eqn += f'{RIGHT_POINTS_COL}{row}, '
-                score_sum_eqn += f'{RIGHT_SCORE_COL}{row}, '
-        points_sum_eqn = points_sum_eqn[:-2] + ')'
-        score_sum_eqn = score_sum_eqn[:-2] + ')'
+        if not teams[team]['score_rows']:
+            points_sum_eqn = ''
+            score_sum_eqn = ''
+        else:
+            points_sum_eqn = '=sum('
+            score_sum_eqn = '=sum('
+            for (side, row) in teams[team]['score_rows']:
+                if side == 'left':
+                    points_sum_eqn += f'{LEFT_POINTS_COL}{row}, '
+                    score_sum_eqn += f'{LEFT_SCORE_COL}{row}, '
+                elif side == 'right':
+                    points_sum_eqn += f'{RIGHT_POINTS_COL}{row}, '
+                    score_sum_eqn += f'{RIGHT_SCORE_COL}{row}, '
+            points_sum_eqn = points_sum_eqn[:-2] + ')'
+            score_sum_eqn = score_sum_eqn[:-2] + ')'
+
         teams[team]['points_sum_eqn'] = points_sum_eqn
         teams[team]['score_sum_eqn'] = score_sum_eqn
 
@@ -251,6 +297,51 @@ def update_score_sheet(wb, groups, teams, quali_rounds):
 
     return
 
+def run_quali_rounds(groups, teams):
+
+    quali_matches = []
+
+    for team in teams:
+        teams[team]['crt5_played'] = 0
+        teams[team]['wait_time'] = 1
+
+    for group in groups:
+        matches = combinations(group, 2)
+        for match in matches:
+            (left, right) = match
+            if teams[left]['ready'] != ABSENT_VALUE and teams[right]['ready'] != ABSENT_VALUE:
+                quali_matches.append(list(match))
+
+    random.shuffle(quali_matches)
+
+    quali_rounds = []
+    round_count = 1
+    while (True):
+        rounds = choose_matches(round_count, teams, quali_matches)
+        if not quali_matches:
+            break
+        update_chosen_teams(teams, quali_matches, rounds)
+        quali_rounds.append(rounds)
+        round_count += 1
+
+    roun1_violation = []
+    roun2_violation = []
+    for seq, rounds in enumerate(quali_rounds):
+        for court, match in enumerate(rounds):
+            if match:
+                left = match[0]
+                right = match[1]
+                if seq == 0:
+                    if teams[left]['ready'] > ROUND1_LATE_TH:
+                        roun1_violation.append(left)
+                    if teams[right]['ready'] > ROUND1_LATE_TH:
+                        roun1_violation.append(right)
+                elif seq == 1:
+                    if teams[left]['ready'] > ROUND2_LATE_TH:
+                        roun2_violation.append(left)
+                    if teams[right]['ready'] > ROUND2_LATE_TH:
+                        roun2_violation.append(right)
+    return round_count, roun1_violation, roun2_violation, quali_rounds
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -259,33 +350,70 @@ if __name__ == "__main__":
 
     excel_file = sys.argv[1]
     wb = openpyxl.load_workbook(filename=excel_file)
-    teams, groups, quali_matches = retrive_teams(wb)
-    quali_rounds = []
-    while (True):
-        rounds = choose_matches(teams, quali_matches)
-        if not any(rounds):
-            break
-        update_chosen_teams(teams, quali_matches, rounds)
-        quali_rounds.append(rounds)
+    teams, groups = retrive_teams(wb)
 
-    for seq, rounds in enumerate(quali_rounds):
+    best_round_count = 100
+    best_round1_violations = [None for i in range(100)]
+    best_round2_violations = [None for i in range(100)]
+    best_rounds = None
+    best_rounds = None
+
+    for run in range(MAX_SELECTION_RUN):
+        round_count, roun1_violation, roun2_violation, quali_rounds = run_quali_rounds(groups, teams)
+        #print(f'round_count {round_count}, roun1_violation {len(roun1_violation)}, roun2_violation {len(roun2_violation)}')
+        if round_count > best_round_count:
+            continue
+        elif round_count < best_round_count:
+            best_round_count = round_count
+            best_rounds = quali_rounds
+            best_round1_violations = roun1_violation
+            best_round2_violations = roun2_violation
+        else:
+            if len(roun1_violation) > len(best_round1_violations):
+                continue
+            elif len(roun1_violation) < len(best_round1_violations):
+                best_rounds = quali_rounds
+                best_round1_violations = roun1_violation
+                best_round2_violations = roun2_violation
+            else:
+                if len(roun2_violation) > len(best_round2_violations):
+                    continue
+                elif len(roun2_violation) < len(best_round2_violations):
+                    best_rounds = quali_rounds
+                    best_round1_violations = roun1_violation
+                    best_round2_violations = roun2_violation
+
+    print(f'Total rounds {best_round_count - 1}')
+    if best_round1_violations:
+        print(f'Round1 ready violations:')
+        for team in best_round1_violations:
+            print(f'  {team} {teams[team]["ready"]}')
+    if best_round2_violations:
+        print(f'Round2 ready violations:')
+        for team in best_round2_violations:
+            print(f'  {team} {teams[team]["ready"]}')
+
+    for seq, rounds in enumerate(best_rounds):
         for court, match in enumerate(rounds):
             if match:
-                teams[match[0]]['rounds'].append((seq+1, court+1))
-                teams[match[1]]['rounds'].append((seq+1, court+1))
-                print(f'({match[0]:6} {match[1]:6}), ', end='')
+                left = match[0]
+                right = match[1]
+                print(f'({left:6} {right:6}), ', end='')
             else:
                 print(f'({"":6} {"":6}), ', end='')
         print()
 
-    update_schedule(wb, teams, quali_rounds)
+
+    update_schedule(wb, teams, best_rounds)
 
     for team in teams:
+        if teams[team]['ready'] == ABSENT_VALUE:
+            continue
         rounds = teams[team]['rounds']
         gaps = []
         for i in range(1, len(rounds)):
             gaps.append(rounds[i][0] - rounds[i-1][0])
 
         print(f'{team}: min {min(gaps)} max {max(gaps)} rounds {" ".join([str(x) for x in rounds])}')
-    update_score_sheet(wb, groups, teams, quali_rounds)
+    update_score_sheet(wb, groups, teams, best_rounds)
     wb.save(excel_file)
---
+++ readme.md
@@ -0,0 +1,46 @@
+Usage of Excel sheet for scheduling and scoring are as follows,
+
+1) Register everyone's attendance in the sheet "attendance" as '1'. If any
+player inform late, enter their ETA in minutes instead of '1'. If any player is
+absent, mark their attendance as '0'.
+
+2) Once enough teams have arrived, run python script. Python script fill sheets
+"quali_schedule" and "quali_score".
+
+3) Share "quali_schedule" sheet to all teams so that they know their turns.
+
+4) Every match results needs to be entered in the sheet "quali_score".
+
+5) Once all qualification rounds are complete, copy group quali results at the
+end of 'quali_score' sheet into 'quali_results' sheet by values only and aligned
+with column 'A' entries of 'quali_results'.
+
+6) In 'quali_results' sheet, every group's results need to be sorted according to
+points and score.
+
+7) Once sorted in step 6), correct player list will be displayed in sheet
+'knockout_score'. Please note that absent team need to be removed manually in
+sheet 'knockout_score'.
+
+8) Complete 'knockout_score' sheet manually.
+
+
+Python script schedule the match as follows,
+1) It will attempt to fill first two rounds with attendance registered teams.
+
+2) No match is scheduled successively for a team.
+
+3) All the Boys & Girls matches are on Court 4.
+
+4) Court 5 will be used only only once for a team.
+
+5) Matches are chosen randomly.
+
+6) There won't be any schedule for absent teams.
+
+Useful commands
+libreoffice --invisible --convert-to html ./bracket.xlsx --outdir /tmp/aa
+xlsx2csv -s 1 ./bracket.xlsx attendance.csv
+
+=if(rand()>=0.5,21,int(rand()*20))
+=if(C4=21,int(rand()*20),21)
